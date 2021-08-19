9ed3ced8988666251eea9ca3ca93dc09ba879429
kavitaparsewar <parsewarkavita@gmail.com>
2021-08-20 00:00:16 +0530

    results stored in dictionary

--- arithmatic.sh
+++ arithmatic.sh
@@ -1,7 +1,25 @@
 #!/bin/bash -x
+
+declare -A results
 echo "enter the value of a,b and c"
 read a b c
 echo $a $b $c
 
-result=$(($a%$b$c))
-echo $result
+r1=$(($a+$b*$c))
+echo $r1
+
+r2=$(($a*$b+$c))
+echo $r2
+
+r3=$(($c+$a/$b))
+echo $r3
+
+r4=$(($a%$b$c))
+echo $r4
+
+results[$r1]="$r1"
+results[$r2]="$r2"
+results[$r3]="$r3"
+results[$r4]="$r4"
+
+echo "All results" ${!results[@]}
